8fe3afbde04107ccef7311c1962738adaadcca2b
Kevin Leptons <kevin.leptons@gmail.com>
2022-07-29 14:31:28 +0700

    set v0.2.3

--- test/client/get_routes.js
+++ test/client/get_routes.js
@@ -118,7 +118,7 @@ describe('Client.getRoutes', () => {
         let source = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
         let target = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
         let routes = await service.getRoutes(source, target, {
-            limit: 10000,
+            limit: 5,
             sourceTokenQuantity: 2,
             targetTokenQuantity: 2
         })
